31dfe02088a517e209017e68dc78a8da7ef80ffa
Fashakin Olashubomi <subomifasakin@outlook.com>
2025-02-25 20:19:13 +0100

    removed dist file

--- dist/lib/redisClient.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.redisClient = void 0;
-const redis_1 = require("redis");
-exports.redisClient = (0, redis_1.createClient)({
-    url: process.env.REDIS_URL,
-});
-// Connect to Redis
-exports.redisClient.connect().catch(console.error);
